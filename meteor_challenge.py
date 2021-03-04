647f480cc0597c9c8747f5a9381ba589ff02d31c
Wellerson Prenholato <wellerson.prenholato@gmail.com>
2021-03-04 12:27:44 -0300

    Update meteor_challenge.py

--- meteor_challenge.py
+++ meteor_challenge.py
@@ -6,6 +6,9 @@ resultImg = img.convert('RGB')
 
 meteors = 0
 stars = 0
+meteorsFallWater = 0
+
+waterAxisX = []
 
 # width, height = img.size
 print(img.width, img.height)
@@ -15,13 +18,21 @@ print(img.width, img.height)
 for i in range(0, img.width):
     for j in range(0, img.height):
         r,g,b = resultImg.getpixel((i,j))
+        if ((r == 0) and (g == 0) and (b== 255)):
+            waterAxisX.append(i)
 
+for i in range(0, img.width):
+    for j in range(0, img.height):
+        r,g,b = resultImg.getpixel((i,j))
         if ((r == 255) and (g == 255) and (b==255)):
             stars += 1
         if ((r == 255) and (g == 0) and (b==0)):
             meteors +=1
+            if(j in waterAxisX):
+                meteorsFallWater += 1
 
 print(f'Estrelas: {stars}')
 print(f'Meteoros: {meteors}')
+print(f'Meteoros caindo na água: {meteorsFallWater}')
 
 # if __name__ == '__main__':
