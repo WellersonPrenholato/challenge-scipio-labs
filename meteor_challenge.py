c459ca48309e9eed0f82751c104aaf155030a4d3
Wellerson Prenholato <wellerson.prenholato@gmail.com>
2021-03-04 15:35:13 -0300

    Update meteor_challenge.py

--- meteor_challenge.py
+++ meteor_challenge.py
@@ -8,6 +8,9 @@ meteors = 0
 stars = 0
 meteorsFallWater = 0
 
+meteorsLine = 0
+starsLine = 0
+
 waterAxisX = []
 
 # width, height = img.size
@@ -18,12 +21,14 @@ print(img.width, img.height)
 for i in range(0, img.width):
     for j in range(0, img.height):
         r,g,b = resultImg.getpixel((i,j))
+
         if ((r == 0) and (g == 0) and (b== 255)):
             waterAxisX.append(i)
 
 for i in range(0, img.width):
     for j in range(0, img.height):
         r,g,b = resultImg.getpixel((i,j))
+
         if ((r == 255) and (g == 255) and (b==255)):
             stars += 1
         if ((r == 255) and (g == 0) and (b==0)):
@@ -34,5 +39,21 @@ for i in range(0, img.width):
 print(f'Estrelas: {stars}')
 print(f'Meteoros: {meteors}')
 print(f'Meteoros caindo na água: {meteorsFallWater}')
+
+# Contagem de meteoros e estrelas por linha
+for i in range(0, img.width):
+    meteorsLine = 0
+    starsLine = 0
+    for j in range(0, img.height):
+        r,g,b = resultImg.getpixel((i,j))
+
+        if ((r == 255) and (g == 255) and (b==255)):
+            starsLine += 1
+        if ((r == 255) and (g == 0) and (b==0)):
+            meteorsLine +=1
+        
+        if(j == img.height -1):
+            print('Linha: ', i, '\nColuna: ', j, '\nStarsLine: ', starsLine, '\nMeteorsline: ', meteorsLine)
+
 
 # if __name__ == '__main__':
