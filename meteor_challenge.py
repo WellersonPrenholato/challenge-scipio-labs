3cecefbccaa38235e2ab69f4ac6d5973a14d7914
Wellerson Prenholato <wellerson.prenholato@gmail.com>
2021-03-05 22:39:01 -0300

    Update meteor_challenge.py

--- meteor_challenge.py
+++ meteor_challenge.py
@@ -1,64 +1,40 @@
 from PIL import Image, ImageFilter
 
-img = Image.open('meteor_challenge_01.png')
+def accountStarsMeteors(img, resultImg):
+    meteors = 0
+    stars = 0
+    meteorsFallWater = 0
 
-resultImg = img.convert('RGB')
+    waterAxisX = []
+    for i in range(0, img.width):
+        for j in range(0, img.height):
+            r,g,b = resultImg.getpixel((i,j))
 
-meteors = 0
-stars = 0
-meteorsFallWater = 0
+            if ((r == 0) and (g == 0) and (b== 255)):
+                waterAxisX.append(i)
 
-meteorsLine = 0
-starsLine = 0
+    for i in range(0, img.width):
+        for j in range(0, img.height):
+            r,g,b = resultImg.getpixel((i,j))
+            # img.putpixel((i,j), (2,119,189)) # Cor Background
 
-waterAxisX = []
+            if ((r == 255) and (g == 255) and (b==255)):
+                stars += 1
+            if ((r == 255) and (g == 0) and (b==0)):
+                meteors +=1
+                if(i in waterAxisX):
+                    meteorsFallWater += 1
+    # Count printing
+    print('### RESULTS ###')            
+    print(f'Stars: {stars}')
+    print(f'Meteors: {meteors}')
+    print(f'Meteors falling in water: {meteorsFallWater}')
 
-# width, height = img.size
-print(img.width, img.height)
+# img.show()
 
-for i in range(0, img.width):
-    for j in range(0, img.height):
-        r,g,b = resultImg.getpixel((i,j))
+if __name__ == '__main__':
 
-        if ((r == 0) and (g == 0) and (b== 255)):
-            waterAxisX.append(i)
+    img = Image.open('meteor_challenge_01.png')
+    resultImg = img.convert('RGB')  
 
-for i in range(0, img.width):
-    for j in range(0, img.height):
-        r,g,b = resultImg.getpixel((i,j))
-
-        if ((r == 255) and (g == 255) and (b==255)):
-            stars += 1
-            # img.putpixel((i,j), (0,0,0,255))
-        if ((r == 255) and (g == 0) and (b==0)):
-            meteors +=1
-            # img.putpixel((i,j), (2,119,189))
-            # img.putpixel((i,j), (0,0,0,255))
-            if(i in waterAxisX):
-                meteorsFallWater += 1
-                # img.putpixel((i,j), (255,255,0))
-
-print(f'Estrelas: {stars}')
-print(f'Meteoros: {meteors}')
-print(f'Meteoros caindo na água: {meteorsFallWater}')
-
-img.show()
-
-# Contagem de meteoros e estrelas por linha
-for i in range(0, img.height):
-    for j in range(0, img.height):
-        r,g,b = resultImg.getpixel((i,j))
-
-        if ((r == 255) and (g == 255) and (b==255)):
-            starsLine += 1
-        if ((r == 255) and (g == 0) and (b==0)):
-            meteorsLine +=1
-        
-        if(j == img.height -1):
-            # print('Linha: ', i, '\nStarsLine: ', starsLine, '\nMeteorsline: ', meteorsLine)
-            print(f'{starsLine}{meteorsLine}')
-
-    meteorsLine = 0
-    starsLine = 0
-
-# if __name__ == '__main__':
+    accountStarsMeteors(img, resultImg)
